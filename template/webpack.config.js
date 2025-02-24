9b2fec97e69a74905ae3cd02e00fb9dc73afbcbb
David Griffon <dgriffon@jahia.com>
2025-02-24 09:50:06 +0100

    chore: rename npm module to javascript module (#80)

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -120,7 +120,7 @@ module.exports = (env, argv) => {
                 path: buildOutput
             },
             externals: {
-                // Those libraries are supplied to webpack at runtime (by the npm-module-engine project), and are not packaged in the output bundle
+                // Those libraries are supplied to webpack at runtime (by the javascript-modules project), and are not packaged in the output bundle
                 '@jahia/javascript-modules-library': 'javascriptModulesLibraryBuilder.getLibrary()',
                 react: 'javascriptModulesLibraryBuilder.getSharedLibrary(\'react\')',
                 'react-i18next': 'javascriptModulesLibraryBuilder.getSharedLibrary(\'react-i18next\')',
